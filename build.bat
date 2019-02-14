d636b362d2619f1e207447ba2f98e3023c8b8a2b
m7b <mbuttig@gmx.net>
2019-02-14 18:35:29 +0100

    cmake

--- build.bat
+++ build.bat
@@ -1,6 +1,7 @@
 @ECHO OFF
 
 SET PATH=%PATH%;C:\CPP\tools\cmake-3.13.3-win64-x64\bin
+SET PATH=%PATH%;C:\Program Files\CMake\bin
 SET PATH=%PATH%;C:\CPP\compiler\i686-8.1.0-release-posix-dwarf-rt_v6-rev0\mingw32\bin
 
 mkdir build
