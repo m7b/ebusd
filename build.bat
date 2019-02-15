0e2b051a97c55896f0e9e2f5f2beb2d5015a9131
m7b <mbuttig@gmx.net>
2019-02-15 11:11:20 +0100

    -10-

--- build.bat
+++ build.bat
@@ -2,8 +2,10 @@
 
 SET PATH=%PATH%;C:\CPP\tools\cmake-3.13.3-win64-x64\bin
 SET PATH=%PATH%;C:\Program Files\CMake\bin
-REM SET PATH=%PATH%;C:\CPP\compiler\i686-8.1.0-release-posix-dwarf-rt_v6-rev0\mingw32\bin
 SET PATH=%PATH%;C:\CPP\compiler\x86_64-8.1.0-release-posix-seh-rt_v6-rev0\mingw64\bin
+
+REM Get MSYS2 libraries (mariadb, boost)
+CALL C:\CPP\tools\msys64\usr\bin\bash -l -c "pacman -Sy --noconfirm --needed mingw64/mingw-w64-x86_64-libmariadbclient mingw64/mingw-w64-x86_64-boost"
 
 mkdir build
 cd build
