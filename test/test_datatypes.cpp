955ab24e9f5aba020c2553b9d1604c12f8b9e671
m7b <mbuttig@gmx.net>
2019-02-15 15:18:28 +0100

    testing test

--- test/test_datatypes.cpp
+++ test/test_datatypes.cpp
@@ -6,16 +6,16 @@
 BOOST_AUTO_TEST_CASE(PassTest)
 {
     float val = 1.23;
-    std::string s_val = ftofrmstdstr(val);
+    //std::string s_val = ftofrmstdstr(val);
 
-    BOOST_CHECK_EQUAL("1.2", s_val);
+    BOOST_CHECK_EQUAL("1.2", val);
 }
 
 BOOST_AUTO_TEST_CASE(FailTest)
 {
     float val = 1.23;
-    std::string s_val = ftofrmstdstr(val);
+    //std::string s_val = ftofrmstdstr(val);
 
-    BOOST_CHECK_NE("1.23", s_val);
-    BOOST_CHECK_NE("1,2", s_val);
+    BOOST_CHECK_NE("1.23", val);
+    BOOST_CHECK_NE("1,2", val);
 }
