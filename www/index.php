b00e7fd168779c38762d85d9cd15c26b57f5ccdb
m7b <mbuttig@gmx.net>
2021-01-11 18:30:56 +0100

    Added year 2021

--- www/index.php
+++ www/index.php
@@ -633,6 +633,94 @@ $(function () {
         }]
 
     });
+	
+	
+    $('#container2021').highcharts({
+
+        data: {
+            csv: document.getElementById('csv2021').innerHTML,
+            parsed: function () {
+                start = +new Date();
+            }
+        },
+
+        chart: {
+            type: 'heatmap',
+            margin: [60, 10, 80, 50]
+        },
+
+        title: {
+            text: 'Highcharts extended heat map',
+            align: 'left',
+            x: 40
+        },
+
+        subtitle: {
+            text: 'Temperature variation by day and hour through 2021',
+            align: 'left',
+            x: 40
+        },
+
+        xAxis: {
+            type: 'datetime',
+            min: Date.UTC(2021, 0, 1),
+            max: Date.UTC(2022, 0, 1),
+            labels: {
+                align: 'left',
+                x: 5,
+                y: 14,
+                format: '{value:%B}' // long month
+            },
+            showLastLabel: false,
+            tickLength: 16
+        },
+
+        yAxis: {
+            title: {
+                text: null
+            },
+            labels: {
+                format: '{value}:00'
+            },
+            minPadding: 0,
+            maxPadding: 0,
+            startOnTick: false,
+            endOnTick: false,
+            tickPositions: [0, 6, 12, 18, 24],
+            tickWidth: 1,
+            min: 0,
+            max: 23,
+            reversed: true
+        },
+
+        colorAxis: {
+            stops: [
+                [0, '#004dca'], /* 3060cf */
+                [0.5, '#fffbbc'],
+                [0.9, '#c4463a'],
+                [1, '#640000'] /* c4463a */
+            ],
+            min: -15,
+            max: 35, /*45,*/
+            startOnTick: false,
+            endOnTick: false,
+            labels: {
+                format: '{value}℃'
+            }
+        },
+
+        series: [{
+            borderWidth: 0,
+            nullColor: '#EFEFEF',
+            colsize: 24 * 36e5, // one day
+            tooltip: {
+                headerFormat: 'Temperature<br/>',
+                pointFormat: '{point.x:%e %b, %Y} {point.y}:00: <b>{point.value} ℃</b>'
+            },
+            turboThreshold: Number.MAX_VALUE // #3404, remove after 4.0.5 release
+        }]
+
+    });
 
     console.log('Rendered in ' + (new Date() - start) + ' ms'); // eslint-disable-line no-console
 
@@ -820,6 +908,9 @@ $(function () {
                 <a href="#chart" onclick=$("#menu-close").click();>BDE/MDE</a>
             </li>
             <li>
+                <a href="#temp2021" onclick=$("#menu-close").click();>Heatmap 2021</a>
+            </li>
+            <li>
                 <a href="#temp2020" onclick=$("#menu-close").click();>Heatmap 2020</a>
             </li>
             <li>
@@ -901,6 +992,19 @@ $(function () {
         </div>
         <!-- /.container -->
     </section>
+  
+	
+  <!-- temp2021 -->
+  <section id="temp2021" class="temp2021">
+      <div class="container">
+          <div class="row">
+              <div class="col-lg-12 text-center">
+              <div id="container2021" style="height: 320px; margin: 0 auto"></div></div>
+          </div>
+          <!-- /.row -->
+      </div>
+      <!-- /.container -->
+  </section>
   
 	
   <!-- temp2020 -->
@@ -1104,6 +1208,10 @@ $(function () {
 
 
 <!-- Source: http://vikjavev.no/ver/highcharts-demos/heatmap.csv.php?year=2013 -->
+
+<pre id="csv2021" style="display: none">Date,Time,Temperature
+<?php include 'https://wolf.mjbsoft.de/wolf_data_temp.php?year=2021'; //need php-opt: allow_url_include = 1 ?>
+</pre>
 
 <pre id="csv2020" style="display: none">Date,Time,Temperature
 <?php include 'https://wolf.mjbsoft.de/wolf_data_temp.php?year=2020'; //need php-opt: allow_url_include = 1 ?>
