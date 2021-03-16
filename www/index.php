ed3222d988d53c3100e4025bb3e245800273a96a
Mark Buttig <mbuttig@gmx.net>
2021-03-16 16:18:50 +0100

    footer

--- www/index.php
+++ www/index.php
@@ -1100,11 +1100,9 @@ $(function () {
     <footer>
         <div class="container">
             <div class="row">
-                <div class="col-lg-10 col-lg-offset-1 text-center">
-                    <h4><strong>mjbsoft</strong>
-                    </h4>
-                    <p>...
-                        <br>Swabian</p>
+                <div class="col-lg-12 text-center">
+                    <h4><strong>mjbsoft</strong></h4>
+                    <p>...<br>Swabian</p>
                     <ul class="list-unstyled">
                         <li><i class="fa fa-phone fa-fw"></i> no phone</li>
                         <li><i class="fa fa-envelope-o fa-fw"></i> <a href="mailto:name@example.com">no email</a>
@@ -1115,11 +1113,10 @@ $(function () {
                     <p class="text-muted">Copyright &copy; mjbsoft.de 2017</p>
                 </div>
             </div>
+            <!-- /.row -->
         </div>
-        <a id="to-top" href="#top" class="btn btn-dark btn-lg"><i class="fa fa-chevron-up fa-fw fa-1x"></i></a>
+        <!-- /.container -->
     </footer>
-
-
 	
 	
 	
